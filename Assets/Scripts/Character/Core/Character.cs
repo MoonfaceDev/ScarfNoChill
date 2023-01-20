d36084aed84af543b2e095be2c8ebc6bf62b2a5b
MoonfaceDev <elaicorem1@gmail.com>
2023-01-20 23:41:37 +0200

    Add scarf, base component and character

---
+++ Assets/Scripts/Character/Core/Character.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Character : BaseComponent
+{
+    public LayerMask groundLayers;
+    public bool Grounded { get; private set; }
+
+    private void Update()
+    {
+        Grounded = Physics2D.OverlapPoint(transform.position, groundLayers);
+    }
+}
