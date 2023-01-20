b2cdb5bd5532e48c6e36cd5e6ce306aadbff127f
MoonfaceDev <elaicorem1@gmail.com>
2023-01-20 18:07:37 +0200

    Add core

---
+++ Assets/Scripts/Character/Core/PlayableBehaviour.cs
@@ -0,0 +1,18 @@
+﻿public abstract class PlayableBehaviour<T> : CharacterBehaviour
+{
+    public abstract bool Playing { get; }
+    
+    public abstract bool CanPlay(T context);
+
+    public void Play(T context)
+    {
+        if (CanPlay(context))
+        {
+            Execute(context);
+        }
+    }
+
+    protected abstract void Execute(T context);
+
+    public abstract void Stop();
+}
