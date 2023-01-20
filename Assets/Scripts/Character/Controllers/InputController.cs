de35d824c138502c512679eab8dccdd6730c5556
MoonfaceDev <elaicorem1@gmail.com>
2023-01-20 18:50:25 +0200

    Add walk

---
+++ Assets/Scripts/Character/Controllers/InputController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class InputController : BaseController
+{
+    private Walk walk;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        walk = GetComponent<Walk>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal != 0)
+        {
+            walk.Play(new WalkContext(horizontal));
+        }
+        else
+        {
+            walk.Stop();
+        }
+    }
+}
