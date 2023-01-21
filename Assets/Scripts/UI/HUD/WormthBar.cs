794c6f87b8098273d5751c835d466ede95716c6d
nitzan-karni <51075167+nitzan-karni@users.noreply.github.com>
2023-01-21 16:15:07 +0200

    added wormth bar

--- Assets/Scripts/UI/HUD/WormthBar.cs
+++ Assets/Scripts/UI/HUD/WormthBar.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Scrollbar))]
 public class WormthBar : MonoBehaviour
 {
     public Wormth wormth;
+    public Scrollbar bar;
+
+    private void Update()
+    {
+        bar.size = wormth.heat / 100;
+    }
 }
