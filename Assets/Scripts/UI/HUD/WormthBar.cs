8f6a7dc10c032720f9aa6937d78b087e56c5ba70
nitzan-karni <51075167+nitzan-karni@users.noreply.github.com>
2023-01-21 15:50:16 +0200

    added wormth and wormthbar

---
+++ Assets/Scripts/UI/HUD/WormthBar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Scrollbar))]
+public class WormthBar : MonoBehaviour
+{
+    public Wormth wormth;
+}
