e55c2271a62fa9fb179dbc7b7f4a6767fb6041a4
nitzan-karni <51075167+nitzan-karni@users.noreply.github.com>
2023-01-21 23:50:55 +0200

    Update SpawnWind.cs

--- Assets/Scripts/Objects/wind/SpawnWind.cs
+++ Assets/Scripts/Objects/wind/SpawnWind.cs
@@ -2,21 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnWind : BaseComponent
+public class SpawnWind : PlayableBehaviour<SpawnWind.Context>
 {
+    public class Context { };
+
     public GameObject wind;
     public Vector2 intervalRange;
 
     public Transform spawnPoint;
 
-    private void Awake()
+    public override bool Playing => Active;
+
+    private bool active;
+
+    private bool Active
     {
+        get => active;
+        set => active = value;
+    }
+
+    public override void Stop()
+    {
+        Active = false;
+    }
+
+    protected override void Execute(Context context)
+    {
+        Active = true;
         StartCoroutine(SpawnWinds());
     }
 
     private IEnumerator SpawnWinds()
     {
-        while (enabled)
+        while (Active)
         {
             float delay = Random.Range(intervalRange.x, intervalRange.y);
             yield return new WaitForSeconds(delay);
